46067d34fb340e1e8fa0d183d099b79414059458
alexwalland <alexanderwalland@gmail.com>
2024-01-16 13:37:21 +0000

    code added to generate readme and generated a basic read me

---
+++ example/readme.md
@@ -0,0 +1,26 @@
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+# Read-me Generator
+ 
+## Table of Contents 
+* [Description](#description) 
+* [License](#license) 
+* [Contact](#contact) 
+
+    ## Description
+  
+    *The what, who, and why*
+  
+    This projects gives prompts to the user and generates a readme based on there responses
+    
+    ## License
+    *The project is protected by the following license*
+  
+    GNU GPLv3
+    ## Contact
+  *You can contact me through the following*
+  
+    Github: alexwalland1997 
+
+    Email: 
+    
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,9 +1,69 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-    return `# ${data.title}
+function generateMarkdown(data, license) {
+  //variable for body of readme
+  let body =
+   `
+    ## Description
   
-  `;
+    *The what, who, and why*
+  
+    ${data.description}
+    
+    `;
+  //create variable for read me and add license badge
+  let gMarkdown = `${license} \n`;
+  
+    gMarkdown +=`# ${data.name} \n`;
+  
+    gMarkdown +=`## Table of Contents \n`;
+  
+    gMarkdown += `* [Description](#description) \n`
+
+  //check if user has entered something if they have add it to table of contents and body
+  if (data.installation !== "") {
+    gMarkdown += `* [Installation](#installation) \n`;
+    body += `## Installation
+    *Instructions on how to install the project and run it*
+  
+      ${data.installation}`;
   }
+
+  if (data.credit !== "") {
+    gMarkdown += `* [Credit](#credit) \n`;
+    body +=`## Credit
+    *All sources used to help with this project*
   
-  module.exports = generateMarkdown;
+      ${data.credit}
+      `;
+  }
+
+  if (data.tests !== "") {
+    gMarkdown += `* [Tests](#tests) \n`;
+    body +=`## Tests
+    *The following are tests that I have run on this project*
   
+      ${data.test}
+      `;
+  }
+
+  gMarkdown += `* [License](#license) \n`;
+  body +=`## License
+    *The project is protected by the following license*
+  
+    ${data.license}
+    `;
+
+  gMarkdown += `* [Contact](#contact) \n`;
+  body +=`## Contact
+  *You can contact me through the following*
+  
+    Github: ${data.github} \n
+    Email: ${data.email}
+    `;
+  console.log(body);
+  gMarkdown += body;
+
+  return gMarkdown;
+}
+
+module.exports = generateMarkdown;
